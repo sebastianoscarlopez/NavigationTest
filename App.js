7976cf8fc8a33ef32f2cb4935851f739dca99107
sebas <sebastianoscarlopz@gmail.com>
2018-05-20 14:27:05 -0300

    Navigation works

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import {DetailsScreen, HomeScreen} from './screens/index';
+
+const RootStack = createStackNavigator({
+  Home: {
+    title:'Home',
+    screen: HomeScreen
+  },
+  Details: {
+    title:'Details',
+    screen: DetailsScreen
+  },
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <RootStack/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/DetailsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+//import Sum from '../components/sum';
+export default class DetailsScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Details Screen</Text>
+          <Button
+            title="Go to Details... again"
+            onPress={() => this.props.navigation.push('Details')}
+          />
+        </View>
+      );
+    }
+  }
