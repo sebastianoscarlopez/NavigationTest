df0760f9a0f283bca915de434b3f63348d661471
sebas <sebastianoscarlopz@gmail.com>
2018-05-20 14:34:07 -0300

    Saved work

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -5,7 +5,7 @@ export default class DetailsScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Details Screen</Text>
+          <Text>Details Screens</Text>
           <Button
             title="Go to Details... again"
             onPress={() => this.props.navigation.push('Details')}
