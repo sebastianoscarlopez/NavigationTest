f6e96b3eedd97b94fce53ee560175a6e30443d4c
sebas <sebastianoscarlopz@gmail.com>
2018-05-20 14:54:48 -0300

    Saved work

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
-//import Sum from '../components/sum';
+import Sum from '../components/sum';
 export default class DetailsScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Details Screens</Text>
+          <Text>Details Screen {Sum(1,2)}</Text>
           <Button
             title="Go to Details... again"
             onPress={() => this.props.navigation.push('Details')}
